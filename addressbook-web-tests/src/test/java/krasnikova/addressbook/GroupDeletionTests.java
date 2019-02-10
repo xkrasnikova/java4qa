f253f07883aa808dde3ef13b5fabef962b74b39f
Kseniya Krasnikova <kseniya.krasnikova@mev.com>
2019-02-10 08:21:44 +0200

    Создала общий базовый класс для тестов, где можно хранить методы, общие для всех тестов

---
+++ addressbook-web-tests/src/test/java/krasnikova/addressbook/GroupDeletionTests.java
@@ -0,0 +1,15 @@
+package krasnikova.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+}
