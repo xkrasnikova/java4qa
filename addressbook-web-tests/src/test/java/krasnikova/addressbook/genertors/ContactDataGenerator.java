954fcd08d566c9f74603794a6bc7d6477956d127
Kseniya Krasnikova <kseniya.krasnikova@mev.com>
2019-03-15 16:33:11 +0200

    Задание №12: Реализовать генераторы тестовых данных

---
+++ addressbook-web-tests/src/test/java/krasnikova/addressbook/genertors/ContactDataGenerator.java
@@ -0,0 +1,65 @@
+package krasnikova.addressbook.genertors;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import krasnikova.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+      ContactDataGenerator generator = new ContactDataGenerator();
+      JCommander jCommander = new JCommander(generator);
+      try {
+        jCommander.parse(args);
+      } catch (ParameterException ex) {
+        jCommander.usage();
+        return;
+      }
+      generator.run();
+    }
+
+    private void run() throws IOException {
+      List<ContactData> contacts = generateContacts(count);
+      saveAsJSON(contacts, new File(file));
+    }
+
+    private void saveAsJSON(List<ContactData> contacts, File file) throws IOException {
+      Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+      String json = gson.toJson(contacts);
+      Writer writer = new FileWriter(file);
+      writer.write(json);
+      writer.close();
+    }
+
+    private List<ContactData> generateContacts(int count) {
+      List<ContactData> contacts = new ArrayList<ContactData>();
+      for (int i = 0; i< count; i++){
+        contacts.add(new ContactData().withFirstName(String.format("FirstName %s", i))
+                .withLastName(String.format("LastName %s", i))
+                .withMobilePhone(String.format("MobilePhone %s", i))
+                .withHomePhone(String.format("HomePhone %s", i))
+                .withWorkPhone(String.format("WorkPhone %s", i))
+                .withEmail(String.format("email1+%s@mail.com", i))
+                .withEmail2(String.format("email2+%s@mail.com", i))
+                .withEmail3(String.format("email3+%s@mail.com", i))
+                .withAddress(String.format("Address %s", i)));
+      }
+      return contacts;
+    }
+}
