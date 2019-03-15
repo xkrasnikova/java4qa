ff09a6370cf94fa5d74e8e89af9070afe6df2c8a
Kseniya Krasnikova <kseniya.krasnikova@mev.com>
2019-03-15 17:17:12 +0200

    Лекция 6.8. Автоматическое закрытие файлов после использования

--- addressbook-web-tests/src/test/java/krasnikova/addressbook/genertors/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/krasnikova/addressbook/genertors/ContactDataGenerator.java
@@ -42,9 +42,9 @@ public class ContactDataGenerator {
     private void saveAsJSON(List<ContactData> contacts, File file) throws IOException {
       Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
       String json = gson.toJson(contacts);
-      Writer writer = new FileWriter(file);
-      writer.write(json);
-      writer.close();
+      try(Writer writer = new FileWriter(file)){
+        writer.write(json);
+      }
     }
 
     private List<ContactData> generateContacts(int count) {
