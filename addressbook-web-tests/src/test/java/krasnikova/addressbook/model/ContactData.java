3d3cd5e6648c8b1bd6a3587529342a9428b3dfc0
Kseniya Krasnikova <kseniya.krasnikova@mev.com>
2019-03-16 14:45:53 +0200

    Лекция 7.3. Условия отбора данных

--- addressbook-web-tests/src/test/java/krasnikova/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/krasnikova/addressbook/model/ContactData.java
@@ -3,45 +3,83 @@ package krasnikova.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
+  @Id
+  @Column(name = "id")
   @XStreamOmitField
   private int id = Integer.MAX_VALUE;
+
   @Expose
+  @Column(name = "firstname")
   private String firstName;
+
   @Expose
+  @Column(name = "lastname")
   private String lastName;
+
   @Expose
+  @Transient
   private String address;
+
   @Expose
+  @Transient
   private String phone;
+
   @Expose
+  @Transient
   private String allPhones;
+
   @Expose
+  @Column(name = "home")
+  @Type(type = "text")
   private String homePhone;
+
   @Expose
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobilePhone;
+
   @Expose
+  @Transient
   private String allEmails;
+
   @Expose
+  @Column(name = "work")
+  @Type(type = "text")
   private String workPhone;
+
   @Expose
+  @Transient
   private String email;
+
   @Expose
+  @Transient
   private String email2;
+
   @Expose
+  @Transient
   private String email3;
+
   @Expose
+  @Transient
   private String group;
+
   @Expose
-  private File photo;
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
@@ -137,7 +175,7 @@ public class ContactData {
   }
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public String getAllEmails() {
--- addressbook-web-tests/src/test/java/krasnikova/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/krasnikova/addressbook/tests/HbConnectionTest.java
@@ -1,5 +1,6 @@
 package krasnikova.addressbook.tests;
 
+import krasnikova.addressbook.model.ContactData;
 import krasnikova.addressbook.model.GroupData;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -36,9 +37,9 @@ public class HbConnectionTest {
   public void testHbConnection(){
     Session session = sessionFactory.openSession();
     session.beginTransaction();
-    List<GroupData> result = session.createQuery( "from GroupData" ).list();
-    for (GroupData group : result) {
-      System.out.println(group);
+    List<ContactData> result = session.createQuery( "from ContactData where deprecated = '0000-00-00'" ).list();
+    for (ContactData contact : result) {
+      System.out.println(contact);
     }
     session.getTransaction().commit();
     session.close();
