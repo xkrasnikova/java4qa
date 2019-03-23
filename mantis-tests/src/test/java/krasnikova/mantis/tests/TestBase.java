0fc93a71e1fefa099fdae47be8479e99c27411f8
Kseniya Krasnikova <kseniya.krasnikova@mev.com>
2019-03-23 11:36:49 +0200

    Добавлен новый модуль для тестов для мантис

---
+++ mantis-tests/src/test/java/krasnikova/mantis/tests/TestBase.java
@@ -0,0 +1,25 @@
+package krasnikova.mantis.tests;
+
+import krasnikova.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser",BrowserType.FIREFOX));
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+
+
+}
