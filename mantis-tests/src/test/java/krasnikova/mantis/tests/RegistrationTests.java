31549a9c065d31bead9c6064bb58ca4baa5c32f6
Kseniya Krasnikova <kseniya.krasnikova@mev.com>
2019-03-23 14:11:31 +0200

    Лекция 8.4. Ленивая инициализация помощников

---
+++ mantis-tests/src/test/java/krasnikova/mantis/tests/RegistrationTests.java
@@ -0,0 +1,12 @@
+package krasnikova.mantis.tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase {
+
+  @Test
+  public void testRegistration() {
+    app.registration().start("user1", "user@test.com");
+  }
+
+}
