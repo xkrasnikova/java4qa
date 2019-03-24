ec1770a4ab973430cae3a6641b6b22071c1e50ce
Kseniya Krasnikova <kseniya.krasnikova@mev.com>
2019-03-24 16:38:31 +0200

    Лекция 8.6. Получение почты на встроенный в тесты почтовый сервер

---
+++ mantis-tests/src/test/java/krasnikova/mantis/model/MailMessage.java
@@ -0,0 +1,12 @@
+package krasnikova.mantis.model;
+
+public class MailMessage {
+
+  public String to;
+  public String text;
+
+  public MailMessage(String to, String text) {
+    this.to = to;
+    this.text = text;
+  }
+}
