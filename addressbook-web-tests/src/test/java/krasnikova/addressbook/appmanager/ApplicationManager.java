72bdc51e3d7996d920a4491de5225e89cd7b2cde
Kseniya Krasnikova <kseniya.krasnikova@mev.com>
2019-04-07 10:58:46 +0300

    добавлена возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/krasnikova/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/krasnikova/addressbook/appmanager/ApplicationManager.java
@@ -1,14 +1,18 @@
 package krasnikova.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -35,10 +39,17 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
-    if (browser.equals(BrowserType.FIREFOX)){
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)){
-      wd = new ChromeDriver();
+    if ("".equals(properties.getProperty("selenium.server"))) {
+
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL (properties.getProperty("selenium.server")), capabilities);
     }
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
